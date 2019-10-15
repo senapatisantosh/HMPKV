8204d4e4bbca5c46d13c136659e0414f8aed357e
Santosh Kumar Senapati <Santosh.Senapati@Pratian.com>
2019-10-15 09:01:40 +0530

    final check in

--- src/app/_helpers/fake-backend.ts
+++ src/app/_helpers/fake-backend.ts
@@ -9,9 +9,7 @@ import { User, Role } from '../_models';
 export class FakeBackendInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const users: User[] = [
-            { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
-            { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User },
-            { id: 3, username: 'santosh', password: 'password@123', firstName: 'Santosh', lastName: 'Senapati', role: Role.User }
+            { id: 1, username: 'krishna', password: 'password@123', firstName: 'Krishna', lastName: '', role: Role.User }
         ];
 
         const authHeader = request.headers.get('Authorization');
